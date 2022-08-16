de52e925fa0c05535c384ad38d99813932bdabd6
sumitgolui <110183327+sumitgolui@users.noreply.github.com>
2022-08-16 15:58:37 +0530

    Time: 39 ms (73.78%), Space: 10.8 MB (44.39%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -7,22 +7,15 @@ public:
         for(int i=0;i<s.length();i++){
             mp[s[i]]++;
         }
-        for(auto it:mp){
-            if(it.second==1){
-                v1.push_back(it.first);
-            }
-            
-        }
-        if(v1.size()==0){
-            return -1;
-        }
-        for(int i=0;i<v1.size();i++){
-            for(int j=0;j<s.length();j++){
-            if(v1[i]==s[j])
-                mini=min(mini,j);
-            }
         
-        }
-        return mini;
+            for(int j=0;j<s.length();j++){
+            if(mp[s[j]]==1)
+                return j;
+                
+            
+            }
+        return -1;
+        
+       
     }
 };
